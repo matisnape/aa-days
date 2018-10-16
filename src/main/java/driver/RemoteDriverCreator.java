55f5c38d5acc52b99d314b13e6686e70d124805f
Anna Anks Nowak <matisnape@wp.pl>
2018-10-16 12:35:02 +0200

    Imports

--- src/main/java/driver/RemoteDriverCreator.java
+++ src/main/java/driver/RemoteDriverCreator.java
@@ -1,6 +1,7 @@
 package driver;
 
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -8,6 +9,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class RemoteDriverCreator {
 
